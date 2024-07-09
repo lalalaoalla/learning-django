113e7b6397e422ed90e84085eb14eefdb6d02302
lalalaoalla <avologina93@mail.ru>
2024-07-09 12:33:06 +0700

    Update views.py
    Именно здесь мне  не удалось создать вторую функцию для возвращения,  здесь мы берем форму из файла forms и сохраняем

--- django_begin/registr/views.py
+++ django_begin/registr/views.py
@@ -1,9 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import UserForm
 
 def registr(request):
     '''Функция вызывается из файла registr/urls.py
     возваращет render, получив от браузера запрос request, далье уже переходит на отображение 
     нашей html-странички'''
+    form = UserForm(request.POST or None)
+    if request.method == 'POST':
+        new_form = form.save()
+        #return redirect('login/')
+        #print(new_form)
     return render(request,'users/register.html', locals())
 
 
