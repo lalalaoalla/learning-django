68807174b923ca62e364025e6bdd294928613938
lalalaoalla <avologina93@mail.ru>
2024-07-10 14:57:16 +0700

    Добавление класса категорий
    Добавила категории продукта, которые должны быть связаны с товарами в каталоге

--- django_begin/catalog/models.py
+++ django_begin/catalog/models.py
@@ -1,17 +1,10 @@
 # #модель - описание таблицы в базе данных
 # #наша база данных(регистрация у нас тут да), будет содеражть данные поля:
-# from django.db import models
+from django.db import models
 
-# class User(models.Model):#типа мы так описываем таблицу, что такое models.Model я пока хз, но внутри описывают поля таблицы
-#     '''model.Model связывает наш класс Users c  бд, также создаст таблицу users, также предоставляет
-#     возможность создания полей с определенными типами, при добавлении нового поля используются 
-#     миграции'''
-#     name = models.CharField(max_length = 100)
-#     firstname = models.CharField(max_length=128)
-#     nickname = models.CharField(max_length=32)
-#     email = models.EmailField()
-#     password = models.CharField(max_length=128)
-
-#     def __str__(self):#в данном случае эта функция показывает, что должно высвечиваться в таблице
-#             return"%s %s никнейм %s" % (self.firstname, self.name, self.nickname)#высвечивает 2(в нашем случае) и более(продолжаем %s) и наши поля после процента
-        
+class CategoryProduct(models.Model):
+    '''Категории наших товаров, они будт связаны с таблицей товаров'''
+    name = models.CharField(max_length=64)
+    
+    def __str__(self):
+        return "%s" % self.name
