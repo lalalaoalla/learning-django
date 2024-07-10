744a3efa79450cd39d7cf67bc25cd0607d42aa6e
lalalaoalla <avologina93@mail.ru>
2024-07-10 16:13:45 +0700

    Добавлены админки
    Добавлены админки для того чтобы можно было смотреть таблички, к сожалению, поля я посмотреть пока не смогла надо побаловаться, где-то я туплю, А ТАКЖЕ ПОСМОТРЕТЬ НА ПОЛЕ on_delete В models.py в связанных полях, там может быть ошибка

--- django_begin/catalog/admin.py
+++ django_begin/catalog/admin.py
@@ -1,26 +1,49 @@
-# from django.contrib import admin
-# from .models import *# из файла odels, который находится в нашей папке импортируем все
+from django.contrib import admin
+from .models import *# из файла odels, который находится в нашей папке импортируем все
 
 
 
 
-# class UserAdmin(admin.ModelAdmin):
+class CategoryProductAdmin(admin.ModelAdmin):
 #     #list_display = ['name', 'firstname','nickname','email','password']# поля для отображения без id
-#     list_display = [field.name for field in User._meta.fields]# это если слишком много полей, и не хочется писать самой
+    list_display = [field.name for field in CategoryProduct._meta.fields]# это если слишком много полей, и не хочется писать самой
 #     exclude = ['email']# это уже при захождении на конкретную запись, те поля, которые мы не хотим смотреть, тое сть их нельзя менять 
 #     #fields = []# то же самое в целом, только какие хотим видеть
 #     list_filter = ['firstname','name',]#в общем это наш фильтр, можно фильтровать по нескольким полям, ГЛАВНОЕ НЕ ЗАБЫТЬ , В КОНЦЕ
 #     search_fields = ['name', 'firstname']# это поиск по какому-то столбцу, можно набрать имя и увидеть всех с эти именем
 
-#     class Meta:
-#         model = User
+    class Meta:
+        model = CategoryProduct
 
+class ProductAdmin(admin.ModelAdmin):
+#     #list_display = ['name', 'firstname','nickname','email','password']# поля для отображения без id
+    list_display = [field.name for field in Product._meta.fields]# это если слишком много полей, и не хочется писать самой
+#     exclude = ['email']# это уже при захождении на конкретную запись, те поля, которые мы не хотим смотреть, тое сть их нельзя менять 
+#     #fields = []# то же самое в целом, только какие хотим видеть
+#     list_filter = ['firstname','name',]#в общем это наш фильтр, можно фильтровать по нескольким полям, ГЛАВНОЕ НЕ ЗАБЫТЬ , В КОНЦЕ
+#     search_fields = ['name', 'firstname']# это поиск по какому-то столбцу, можно набрать имя и увидеть всех с эти именем
+
+    class Meta:
+        model = Product
+
+
+class ImagesInProductAdmin(admin.ModelAdmin):
+#     #list_display = ['name', 'firstname','nickname','email','password']# поля для отображения без id
+    list_display = [field.name for field in ImagesInProduct._meta.fields]# это если слишком много полей, и не хочется писать самой
+#     exclude = ['email']# это уже при захождении на конкретную запись, те поля, которые мы не хотим смотреть, тое сть их нельзя менять 
+#     #fields = []# то же самое в целом, только какие хотим видеть
+#     list_filter = ['firstname','name',]#в общем это наш фильтр, можно фильтровать по нескольким полям, ГЛАВНОЕ НЕ ЗАБЫТЬ , В КОНЦЕ
+#     search_fields = ['name', 'firstname']# это поиск по какому-то столбцу, можно набрать имя и увидеть всех с эти именем
+
+    class Meta:
+        model = ImagesInProduct
 # admin.site.register(User, UserAdmin)#типа наш класс UserAdmin перезапишет с теми полями, которые нам нужны
 
 # #ЕСЛИ СИЛЬНО ЗАХОЧЕТСЯ:
 
 # #выводит лишь одно единственное поле - users, в файле registr/models.py - написано то, что мы хотим
 # #выводить в этом поле
-# #admin.site.register(User)#берем в админ сайт, регистр, говорим, что мы хотим зарегистрироваться
-
+# admin.site.register(CategoryProduct)#берем в админ сайт, регистр, говорим, что мы хотим зарегистрироваться
+# admin.site.register(Product)
+# admin.site.register(ImagesInProduct)
 
--- django_begin/orders/admin.py
+++ django_begin/orders/admin.py
@@ -1,26 +1,48 @@
-# from django.contrib import admin
-# from .models import *# из файла odels, который находится в нашей папке импортируем все
+from django.contrib import admin
+from .models import *# из файла odels, который находится в нашей папке импортируем все
 
 
 
 
-# class UserAdmin(admin.ModelAdmin):
+class StatusAdmin(admin.ModelAdmin):
 #     #list_display = ['name', 'firstname','nickname','email','password']# поля для отображения без id
-#     list_display = [field.name for field in User._meta.fields]# это если слишком много полей, и не хочется писать самой
+    list_display = [field.name for field in Status._meta.fields]# это если слишком много полей, и не хочется писать самой
 #     exclude = ['email']# это уже при захождении на конкретную запись, те поля, которые мы не хотим смотреть, тое сть их нельзя менять 
 #     #fields = []# то же самое в целом, только какие хотим видеть
 #     list_filter = ['firstname','name',]#в общем это наш фильтр, можно фильтровать по нескольким полям, ГЛАВНОЕ НЕ ЗАБЫТЬ , В КОНЦЕ
 #     search_fields = ['name', 'firstname']# это поиск по какому-то столбцу, можно набрать имя и увидеть всех с эти именем
 
-#     class Meta:
-#         model = User
+    class Meta:
+        model = Status
 
+class MakingAnOrderAdmin(admin.ModelAdmin):
+#     #list_display = ['name', 'firstname','nickname','email','password']# поля для отображения без id
+    list_display = [field.name for field in MakingAnOrder._meta.fields]# это если слишком много полей, и не хочется писать самой
+#     exclude = ['email']# это уже при захождении на конкретную запись, те поля, которые мы не хотим смотреть, тое сть их нельзя менять 
+#     #fields = []# то же самое в целом, только какие хотим видеть
+#     list_filter = ['firstname','name',]#в общем это наш фильтр, можно фильтровать по нескольким полям, ГЛАВНОЕ НЕ ЗАБЫТЬ , В КОНЦЕ
+#     search_fields = ['name', 'firstname']# это поиск по какому-то столбцу, можно набрать имя и увидеть всех с эти именем
+
+    class Meta:
+        model = MakingAnOrder
+
+class ProductInBasketAdmin(admin.ModelAdmin):
+#     #list_display = ['name', 'firstname','nickname','email','password']# поля для отображения без id
+    list_display = [field.name for field in ProductInBasket._meta.fields]# это если слишком много полей, и не хочется писать самой
+#     exclude = ['email']# это уже при захождении на конкретную запись, те поля, которые мы не хотим смотреть, тое сть их нельзя менять 
+#     #fields = []# то же самое в целом, только какие хотим видеть
+#     list_filter = ['firstname','name',]#в общем это наш фильтр, можно фильтровать по нескольким полям, ГЛАВНОЕ НЕ ЗАБЫТЬ , В КОНЦЕ
+#     search_fields = ['name', 'firstname']# это поиск по какому-то столбцу, можно набрать имя и увидеть всех с эти именем
+
+    class Meta:
+        model = ProductInBasket
 # admin.site.register(User, UserAdmin)#типа наш класс UserAdmin перезапишет с теми полями, которые нам нужны
 
 # #ЕСЛИ СИЛЬНО ЗАХОЧЕТСЯ:
 
 # #выводит лишь одно единственное поле - users, в файле registr/models.py - написано то, что мы хотим
 # #выводить в этом поле
-# #admin.site.register(User)#берем в админ сайт, регистр, говорим, что мы хотим зарегистрироваться
-
+# admin.site.register(Status)#берем в админ сайт, регистр, говорим, что мы хотим зарегистрироваться
+# admin.site.register(MakingAnOrder)
+# admin.site.register(ProductInBasket)
 
