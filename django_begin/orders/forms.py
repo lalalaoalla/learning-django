258d943ca0e47377c774db3d9151cc8af1a15079
lalalaoalla <avologina93@mail.ru>
2024-07-12 19:22:55 +0700

    Update forms.py
    Добавила форму, правда где-то ошибка, но такая уже была, надо глянуть

--- django_begin/orders/forms.py
+++ django_begin/orders/forms.py
@@ -1,5 +1,20 @@
-# from django import forms 
-# from .models import *
+from django import forms 
+from .models import *
+
+
+class MakingAnOrderForm(forms.ModelForm):
+    name = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control py-4', 'placeholder': 'Введите имя'}))
+    firstname = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control py-4', 'placeholder': 'Введите фамилию'}))
+    email = forms.CharField(widget=forms.EmailInput(attrs={
+        'class': 'form-control py-4', 'placeholder': 'you@example.com'}))
+    address = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control py-4', 'placeholder': 'Россия, Москва, ул. Мира 6'}))
+    
+    class Meta:
+        model = MakingAnOrder
+        fields = ['name','firstname','email', 'address']
 
 # class UserForm(forms.ModelForm):
 
