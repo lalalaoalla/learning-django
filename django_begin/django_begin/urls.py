ddccb24743219fe109d38459ee7f1cb77fbd1d44
lalalaoalla <avologina93@mail.ru>
2024-07-15 21:16:11 +0700

    Автоматические добавления или фото

--- django_begin/django_begin/urls.py
+++ django_begin/django_begin/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.conf.urls import include
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 #from django.contrib.auth import views as auth_views
 
 admin.autodiscover()#типа строчка для того, чтобы мы смогли добавлять наши таблицы
@@ -29,4 +31,6 @@ urlpatterns = [
     path('registr/',include('registr.urls')),
     path('orders/',include('orders.urls')),
     path('my_profile/',include('my_profile.urls')),
-]
+]  \
++static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
++static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
