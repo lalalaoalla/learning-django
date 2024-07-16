2db88f8740206c5e5231783b0a7fb206764c2553
lalalaoalla <avologina93@mail.ru>
2024-07-16 15:56:01 +0700

    Наброски авторизации
    Пока сделаны наброски авторизации, даже в html не сделано еще ничего, приеду потом вечером сяду доделаю

---
+++ django_begin/login/forms.py
@@ -0,0 +1,9 @@
+from django.contrib.auth.forms import AuthenticationForm 
+from django import forms
+
+class LoginForm(AuthenticationForm):
+    nickname = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control py-4', 'placeholder': 'Введите имя пользователя'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': 'form-control py-4', 'placeholder': 'Введите пароль'}))
+    pass
