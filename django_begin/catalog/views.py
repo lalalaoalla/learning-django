4d47ac9e92eb82280c313b06c15d6e19e35a74c4
lalalaoalla <avologina93@mail.ru>
2024-07-16 15:37:23 +0700

    Update views.py
    Сделано все считывание по категориям+ пагинация

--- django_begin/catalog/views.py
+++ django_begin/catalog/views.py
@@ -1,31 +1,49 @@
 from django.shortcuts import render
 from .models import *
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+
 
 def catalog(request):
     '''Функция вызывается из файла catalog/urls.py
     возваращет render, получив от браузера запрос request, далье уже переходит на отображение 
     нашей html-странички'''
-    
-    category_id = request.GET.get('category')#строка в браузере
+    #номер нашей категории в строке в браузере(после "?")
+    category_id = request.GET.get('category')
 
-    category_one = category_id==1#пока не рабочая фигня
-    category_two = category_id==2
+    categories = CategoryProduct.objects.all()# переменная, считавшая все категории
+    products = Product.objects.filter(is_active=True)# переменная для продуктов(опять же она не особо нужна,наверное, для считывания)
+    product_images = ImagesInProduct.objects.all()#переменная, считавшая продукты
 
-    categories = CategoryProduct.objects.all()#для считывания названий категорий
+    #Если у нас есть категории айди
+    if category_id:
+        #то наша category_products  получает продукты считанные с этим айди
+        category_products = product_images.filter(product__category__id=category_id)
+    else:
+        #иначе - считываем наши новинки
+        category_products = product_images.filter(product__category__id=1)
 
-    products = Product.objects.filter(is_active=True)#сейчас уже не нужен вроде но пусть будет
+    #для пагинации
+    paginator = Paginator(category_products, 6)# то есть, у нас на странице будет 6 товаров
+    page_number = request.GET.get('page')#получаем номер нашей страницы
 
-    product_images = ImagesInProduct.objects.all()#для считывания товаров и фоток и всего их
+    try:
+        page = paginator.page(page_number)#указание на страницу
+    except PageNotAnInteger:#это если что как бы название ошибки, да, раньше я не использовала
+        # Если номер страницы не целое число, используйте первую страницу
+        page = paginator.page(1)
+    except EmptyPage:
+        # Если номер страницы слишком большой, используйте последнюю страницу
+        page = paginator.page(paginator.num_pages)
 
-    category_new_products = product_images.filter(product__category__id=1)# переменные для использования категорий товаов
-    category_clothes = product_images.filter(product__category__id=2)
-
-    #title = 'Store-каталог'
-    
-    context = {'page_title' : 'Store-каталог','categories' : categories, 'products' : products, 'product_images' : product_images,
-               'category_clothes' : category_clothes,'category_new_products' : category_new_products,
-               'category_one' : category_one, 'category_two' : category_two}
-    return render(request,'products.html', context)
+    context = {
+        'page_title': 'Store-каталог',
+        'categories': categories,
+        'products': products,
+        'product_images': product_images,
+        'category_products': category_products,
+        'page' : page,
+    }
+    return render(request, 'products.html', context)
 #Почему-то с отдельными функциями не работает, может ей не нравится, что я ссылаюсь на одну и ту же страницу
 #что вполне может быть
 
